9c866c5606ac752f82ba285d6420f6355b5ff02a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-15 09:48:33 -0300

    add users list using prisma orm

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from "express";
+import { prisma } from "../prisma";
 
 class UsersController {
     async index(request: Request, response: Response) {
-        return response.json();
+        // Função parecida com select do sql ou knex list do query builder porem em orm com prisma em modelo de objeto
+        const users = await prisma.user.findMany();
+
+        return response.json(users);
     }
 
     async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        // Função parecida com insert into do sql ou knex create do query builder porem em orm com prisma em modelo de objeto
+        await prisma.user.create({ data: { name, email } });
+
         return response.status(201).json();
     }
 
-    async show(request: Request, response: Response) {
-        return response.json();
-    }
+    async show(request: Request, response: Response) {}
 }
 
 export { UsersController };
